1e5908548b0b30a79898ce32cfd83b46f278ba64
Kresimir Ivanjko <tp8pbrzyr4@privaterelay.appleid.com>
2023-05-16 19:33:44 +0200

    3

--- Dota2Heroes/ViewController.swift
+++ Dota2Heroes/ViewController.swift
@@ -9,110 +9,113 @@ import UIKit
 
 class ViewController: UIViewController {
     var heroes = [Hero]()
+    var filteredHeroes = [Hero]()
     
     @IBOutlet var myTableView: UITableView!
+    @IBOutlet var searchHero: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchHeroStats()
-    
+        searchHero.delegate = self
     }
     
     func fetchHeroStats() {
         let url = URL(string: "https://api.opendota.com/api/heroStats")!
-       
-        URLSession.shared.dataTask(with: url) { data, response, error in
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, error in
             guard let data = data, error == nil else {
                 print(error?.localizedDescription ?? "Unknown error")
                 return
             }
             do {
-                self.heroes = try JSONDecoder().decode([Hero].self, from: data)
-               DispatchQueue.main.async {
-                  self.myTableView.reloadData()
-               }
+                self?.heroes = try JSONDecoder().decode([Hero].self, from: data)
+                self?.filteredHeroes = self?.heroes ?? []
+                DispatchQueue.main.async {
+                    self?.myTableView.reloadData()
+                }
             } catch {
                 print(error.localizedDescription)
             }
         }.resume()
     }
 }
+
 extension UIImageView {
-    
     func downloadImage(from url: URL) {
-        
         contentMode = .scaleToFill
-        let dataTask = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
-            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200,
-                  let mimeType = response?.mimeType, mimeType.hasPrefix("img"),
-                    let data = data, error == nil,
-                  let img = UIImage(data: data)
-                    else
-                    {
-                    return
-                    }
-            DispatchQueue.main.async {
-                self.image = img
+        let dataTask = URLSession.shared.dataTask(with: url) { [weak self] (data, _, error) in
+            guard let data = data, error == nil else {
+                return
             }
-        })
+            if let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            }
+        }
         dataTask.resume()
     }
 }
 
 extension ViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return heroes.count
-        
+        return filteredHeroes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = myTableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! HeroTableViewCell
-        cell.localizedName.text = heroes[indexPath.row].localized_name
-        cell.primaryAttr.text = heroes[indexPath.row].primary_attr
-        cell.attackType.text = heroes[indexPath.row].attack_type
-        let hero = heroes[indexPath.row]
+        let hero = filteredHeroes[indexPath.row]
+        cell.localizedName.text = hero.localized_name
+        cell.primaryAttr.text = hero.primary_attr
+        cell.attackType.text = hero.attack_type
         cell.configureAttributeImage(for: hero)
-        fetchHeroImages(for: hero) { image in
-            DispatchQueue.main.async {
-                cell.heroImage.image = image
-             
-            
-            }
-        }
+        if let heroName = hero.localized_name?.lowercased().replacingOccurrences(of: " ", with: "_").replacingOccurrences(of: "-", with: "") {
+              let imageUrl = "https://api.opendota.com/apps/dota2/images/heroes/\(heroName)_full.png"
+              if let url = URL(string: imageUrl) {
+                  DispatchQueue.global().async {
+                      if let data = try? Data(contentsOf: url) {
+                          DispatchQueue.main.async {
+                              cell.heroImage.image = UIImage(data: data)
+                          }
+                      }
+                  }
+              }
+          }
         return cell
-        
-        
-        
-        
-        func fetchHeroImages(for hero: Hero, completion: @escaping (UIImage?) -> Void) {
-            guard let heroName = hero.localized_name else {
-                completion(nil)
-                return
-            }
-            
-            let heroImageName = heroName.lowercased().replacingOccurrences(of: " ", with: "_").replacingOccurrences(of: "-", with: "")
-            let urlString = "https://api.opendota.com/apps/dota2/images/dota_react/heroes/\(heroImageName).png"
-            
-            guard let url = URL(string: urlString) else {
-                completion(nil)
-                return
-            }
-            
-            URLSession.shared.dataTask(with: url) { data, _, error in
-                if let error = error {
-                    print(error.localizedDescription)
-                    completion(nil)
-                    return
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            // If search text is empty, show all heroes
+            filteredHeroes = heroes
+        } else {
+            // Filter the heroes array based on consecutive letters typed
+            filteredHeroes = heroes.filter { hero in
+                guard let name = hero.localized_name?.lowercased() else {
+                    return false
                 }
                 
-                if let data = data, let image = UIImage(data: data) {
-                    completion(image)
-                } else {
-                    completion(nil)
+                var currentIndex = name.startIndex
+                let searchText = searchText.lowercased()
+                
+                for char in searchText {
+                    let range = currentIndex..<name.endIndex
+                    guard let foundRange = name.range(of: String(char),
+                                                      options: .caseInsensitive,
+                                                      range: range,
+                                                      locale: nil) else {
+                        return false
+                    }
+                    currentIndex = foundRange.upperBound
                 }
-            }.resume()
+                
+                return true
+            }
         }
         
+        myTableView.reloadData()
     }
-    
 }
